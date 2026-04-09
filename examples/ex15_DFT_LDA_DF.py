4dd6fc34d8b96f4388f64d9eca5e9c9a1baf882b
Manas Sharma <manas.sharma@uni-jena.de>
2026-04-10 02:35:03 +0530

    update requirements and ex15

--- examples/ex15_DFT_LDA_DF.py
+++ examples/ex15_DFT_LDA_DF.py
@@ -26,36 +26,16 @@ funcidpyfock = 'SPZ' # [1, 9]
 # funcidpyfock = 'SPW' # [1, 12]
 
 
-# basis_set_name = 'sto-2g'
+
 # basis_set_name = 'sto-3g'
-# basis_set_name = 'sto-6g'
-# basis_set_name = '6-31G'
 basis_set_name = 'def2-SVP'
-# basis_set_name = 'def2-DZVP'
 # basis_set_name = 'def2-TZVP'
-# basis_set_name = 'def2-TZVPP'
-# basis_set_name = 'def2-TZVPPD'
-# basis_set_name = 'def2-QZVPPD'
-# basis_set_name = 'cc-pVDZ'
+
 
 auxbasis_name = 'def2-universal-jfit'
 
-# xyzFilename = 'Benzene-Fulvene_Dimer.xyz'
-# xyzFilename = 'Zn.xyz'
-# xyzFilename = 'Zn_dimer.xyz'
-# xyzFilename = 'TPP.xyz'
-# xyzFilename = 'Zn_TPP.xyz'
+
 xyzFilename = 'h2o.xyz'
-# xyzFilename = 'Ethane.xyz'
-# xyzFilename = 'Cholesterol.xyz'
-# xyzFilename = 'Serotonin.xyz'
-# xyzFilename = 'Decane_C10H22.xyz'
-# xyzFilename = 'Icosane_C20H42.xyz'
-# xyzFilename = 'Tetracontane_C40H82.xyz'
-# xyzFilename = 'Pentacontane_C50H102.xyz'
-# xyzFilename = 'Octacontane_C80H162.xyz'
-# xyzFilename = 'Hectane_C100H202.xyz'
-# xyzFilename = 'Icosahectane_C120H242.xyz'
 
 
 # Initialize a Mol object 
@@ -73,5 +53,4 @@ dftObj.conv_crit = 1e-7
 dftObj.max_itr = 20
 dftObj.ncores = ncores
 dftObj.save_ao_values = True # Requires more memory but is faster
-dftObj.isDF = True # Use density fitting
 energyCrysX, dmat = dftObj.scf()
